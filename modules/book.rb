c7d35f3e6946f334a3784c57fbeaacd230521a8c
Glenda Diaz <glendadiazah@outlook.com>
2023-03-20 17:35:50 -0600

    Add tests for book and label methods

--- modules/book.rb
+++ modules/book.rb
@@ -1,13 +1,13 @@
 require_relative '../item'
 class Book < Item
-    def initialize(publisher:, cover_state:)
-        super(id, publish_date, archived)
+    def initialize(id:, publish_date:, archived:, publisher:, cover_state:)
+        super(id: id, publish_date: publish_date, archived: archived)
         @publisher = publisher
         @cover_state = cover_state
     end
 
     def can_be_archived?
-        if can_be_archived? || cover_state == 'bad'
+        if @cover_state == 'bad'
             true
         else
             false
