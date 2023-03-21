768d28d3f168ab73bd11ac44e961d5a1ae7b2315
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-20 23:51:45 -0600

    Create Movie class that inherits from Item class

---
+++ modules/movie.rb
@@ -0,0 +1,12 @@
+class Movie < Item
+  def initialize(publish_date:, silent:)
+    super(publish_date:)
+    @silent = silent
+  end
+
+  def can_be_archived?
+    return true if super && @silent == true
+
+    false
+  end
+end
