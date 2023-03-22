3a02be53d779f4601331c82989ff7bdb517ed465
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-22 09:11:10 -0600

    Update author & game tests

--- lib/game.rb
+++ lib/game.rb
@@ -5,8 +5,8 @@ require 'securerandom'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(archived, publish_date, multiplayer, last_played_at)
-    super(id: SecureRandom.uuid, publish_date: publish_date, archived: archived)
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date: publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
