891b8494ba29a3ce812d451f0aa3eb73ca397ee8
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-22 11:41:15 -0600

    Timeproof existing Movie date methods

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -2,13 +2,31 @@ require 'rspec'
 require_relative '../lib/movies/movie'
 
 RSpec.describe Movie do
-  subject { described_class.new(publish_date: '2022-01-01', silent: true) }
-
   describe '#initialize' do
+    subject { described_class.new(publish_date: '1997-01-22', silent: true) }
+
     it 'creates a new movie with publish date and silent vars' do
       expect(subject).to be_instance_of(described_class)
-      expect(subject.publish_date).to eql('2022-01-01')
+      expect(subject.publish_date).to eql('1997-01-22')
       expect(subject.silent).to eql(true)
+    end
+  end
+
+  describe '#can_be_archived?' do
+    context 'when the movie is less than 10 years old and is NOT silent' do
+      subject { described_class.new(publish_date: Date.today.to_s, silent: false) }
+
+      it 'returns false' do
+        expect(subject.send(:can_be_archived?)).to be false
+      end
+    end
+    
+    context 'when the movie is more than 10 years old and is NOT silent' do
+      subject { described_class.new(publish_date: '1997-01-17', silent: false) }
+
+      it 'returns true' do
+        expect(subject.send(:can_be_archived?)).to be true
+      end
     end
   end
 end
