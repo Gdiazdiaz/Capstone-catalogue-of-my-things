aad73e88cec4cf36871a99442a3faf332e16cb0f
GutemaG <bir13gud@gmail.com>
2023-03-22 08:57:52 +0300

    Update author and game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,22 @@
 require_relative '../item'
 require 'date'
+require 'securerandom'
 
 class Game < Item
-  def initialize(multiplayer, last_played_at)
-    super(multiplayer, last_played_at)
+  attr_accessor :multiplayer, :last_played_at
+  
+  def initialize(archived, publish_date, multiplayer, last_played_at)
+    super(id: SecureRandom.uuid, publish_date: publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
+
+  private
+
   def can_be_archived?
     super() && year_since_last_played_at > 2
   end
-
-  private
 
   def year_since_last_played_at
     d = (Date.today - Date.parse(last_played_at)) / 365
