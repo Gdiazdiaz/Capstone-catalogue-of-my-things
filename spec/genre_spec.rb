5562b99f6927df3add1d3283d7926ef40cf5c321
belaymit <2belamit@gmail.com>
2023-03-23 11:32:42 +0300

    Added test cases for genre

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,6 +1,6 @@
 require_relative './helper_spec'
 
-describe 'Genre class test cases' do
+describe 'Test cases for Genre class' do
   before(:each) do
     @genre = Genre.new('music')
   end
@@ -9,6 +9,26 @@ describe 'Genre class test cases' do
     it 'Should takes one parameter and the return a genre object' do
       @genre = Genre.new('music')
       expect(@genre).to be_instance_of(Genre)
+    end
+  end
+  context 'Invoking genre_object.name' do
+    it 'returns string' do
+      @genre = Genre.new('music')
+      expect(@genre.name).to be_kind_of(String)
+    end
+
+    it 'Should return the correct name' do
+      @genre = Genre.new('music')
+      expect(@genre.name).to eql 'music'
+    end
+  end
+
+  context "Invoking genre_object.add_item(album_item) method\n" do
+    it 'Should add "album_item" into "genre_object.items" array' do
+      album_item = MusicAlbum.new('2020-10-10', on_spotify: true)
+      @genre = Genre.new('music')
+      @genre.add_item(album_item)
+      expect(@genre.items).to include(album_item)
     end
   end
 end
