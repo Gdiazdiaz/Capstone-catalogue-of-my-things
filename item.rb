f6f94ee233b8a2fda746594c795219c1c4c3bbd5
belaymit <2belamit@gmail.com>
2023-03-23 11:30:16 +0300

    Added modules to list album and genre

--- item.rb
+++ item.rb
@@ -2,16 +2,24 @@ require 'securerandom'
 require 'date'
 
 class Item
-  attr_reader :id
+  # attr_reader :id
+  # attr_accessor :genre, :author, :label, :source, :publish_date, :archived
 
-  def initialize(publish_date:)
-    @id = SecureRandom.hex(10)
+  # def initialize(publish_date = Date.today,  archived: false)
+  #   @id = SecureRandom.hex(10)
+  #   @publish_date = publish_date
+  #   @archived = archived
+  #   @source = nil
+  # end
+  attr_accessor :publish_date, :archived
+  attr_reader :id, :genre, :author, :label
+
+  def initialize(publish_date = Date.today, id = SecureRandom.hex(10), archived: false)
+    @id = id
     @publish_date = publish_date
-    @archived = false
+    @archived = archived
     @source = nil
   end
-
-  attr_accessor :genre, :author, :label, :source, :publish_date, :archived
 
   def move_to_archive
     @archived = true if can_be_archived?
@@ -22,6 +30,21 @@ class Item
     return if source.nil?
 
     source.items.push(self) unless source.items.include?(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
   end
 
   private
